7f41912fec5a150abb33d6ab115c1250ad92c33e
Gerald Yang <gerald.yang@canonical.com>
2023-08-07 13:28:27 +0800

    Remove unused files and ask if GO needs to be installed

--- install-deb.sh
+++ install-deb.sh
@@ -23,35 +23,44 @@ if [ -z "$setup_path" ]; then
         echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
 fi
 
-setup_gopath=$(grep 'export GOPATH' ~/.bashrc)
+echo "Install GO? y or n: "
+read INSTALL_GO
+if [ "$INSTALL_GO" = "y" ] ; then
+        setup_gopath=$(grep 'export GOPATH' ~/.bashrc)
 
-if [ -z "$setup_gopath" ]; then
-        echo 'export GOPATH=$(go env GOPATH)' >> ~/.bashrc
+        if [ -z "$setup_gopath" ]; then
+                echo 'export GOPATH=$(go env GOPATH)' >> ~/.bashrc
+        fi
+
+        echo "Install go packages"
+        go install golang.org/x/tools/gopls@latest
+        go install github.com/jstemmer/gotags@latest
+        go install golang.org/x/tools/cmd/goimports@latest
+        go install honnef.co/go/tools/cmd/staticcheck@latest
+        go install github.com/fatih/motion@latest
+        go install github.com/nsf/gocode@latest
+        go install golang.org/x/tools/cmd/guru@latest
+        go install golang.org/x/tools/cmd/gorename@latest
+        go install github.com/rogpeppe/godef@latest
+        go install github.com/kisielk/errcheck@latest
+        go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
+        go install github.com/josharian/impl@latest
+        go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+        go install github.com/fatih/gomodifytags@latest
+        go install honnef.co/go/tools/cmd/keyify@latest
+        go install golang.org/x/lint/golint@latest
 fi
-
-go install golang.org/x/tools/gopls@latest
-go install github.com/jstemmer/gotags@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install github.com/fatih/motion@latest
-go install github.com/nsf/gocode@latest
-go install golang.org/x/tools/cmd/guru@latest
-go install golang.org/x/tools/cmd/gorename@latest
-go install github.com/rogpeppe/godef@latest
-go install github.com/kisielk/errcheck@latest
-go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
-go install github.com/josharian/impl@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go install github.com/fatih/gomodifytags@latest
-go install honnef.co/go/tools/cmd/keyify@latest
-go install golang.org/x/lint/golint@latest
 
 echo "-------------------------------------------------------------------"
 echo "Please perform 'PlugInstall' in vim to install all plugins"
 echo "Fix gtags issue: chmod +x ~/.vim/plugged/vim-gutentags/plat/unix/update_gtags.sh"
 echo "For C++, please enable YouCompleteMe plugin in vimrc and compile it:"
 echo "goto ~/.vim/plugged/YouCompleteMe and run 'python3 install.py --clang-completer'"
-echo "For Golang plugin"
-echo "modify ~/.vim/plugged/vim-go/ftplugin/go/commands.vim"
-echo "comment out 'if go#package#InGOPATH()', this command gets error"
+
+if [ "$INSTALL_GO" = "y" ] ; then
+        echo "For Golang plugin"
+        echo "modify ~/.vim/plugged/vim-go/ftplugin/go/commands.vim"
+        echo "comment out 'if go#package#InGOPATH()', this command gets error"
+fi
+
 echo "-------------------------------------------------------------------"
